f75404e8b05a3d2c57d09cb67540a99bc43d1a8d
cveimarsena <vcelis@sena.edu.co>
2026-02-17 07:03:12 -0500

    Final

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -110,10 +110,196 @@
     #t_4_1.titulo-segundo.color-acento-contenido(data-aos="fade-left")
       h2  4.2	Comunicación asertiva y efectiva
 
+    .row.justify-content-center.align-items-center.mb-4
+      .col-lg-3.col-6.mb-4.mb-lg-0
+        figure(data-aos="zoom-in")
+          img.img-fluid(src="@/assets/curso/temas/tema4/img11.png" alt="Icono 1")
+      .col-lg-9.mb-4.mb-lg-0
+        .row.justify-content-center.mb-3
+          .col-lg-1.d-none.d-lg-block
+            figure(data-aos="zoom-in")
+              img.img-fluid(src="@/assets/curso/temas/tema4/img12.svg" alt="Icono 1")
+          .col-lg-11
+            p.mb-0(data-aos="fade-left") Es fundamental fortalecer las habilidades sociales, entre ellas la comprensión de la importancia de la comunicación en la atención en salud. El conocimiento de los elementos de la comunicación, los medios, las barreras y las habilidades comunicativas, favorece el desarrollo de una comunicación asertiva y efectiva por parte del talento humano en salud.
+        .cajon.color-secundario.p-4.mb-4
+          p.mb-0(data-aos="fade-left") La comunicación depende en gran medida de la calidad de la relación interpersonal y va más allá del lenguaje oral o de la simple transmisión de información. Incluye aspectos como el modo de vestir, los gestos, las expresiones faciales, el tono de voz e incluso el silencio, los cuales permiten expresar emociones como agrado, desagrado, alegría o tristeza. En este sentido, la comunicación integra el conocimiento del comportamiento humano para comprender de manera integral el acto comunicativo.
+        p.mb-0 Entendida como un intercambio de información, ideas y emociones, la comunicación implica una relación interpersonal en la que el mensaje debe ser comprendido, interpretado y respondido. Este proceso es esencial para establecer vínculos, generar confianza y favorecer interacciones efectivas y humanizadas en los escenarios de atención en salud.
 
+    .row.justify-content-center.align-items-center.mb-4
+      .col-lg-7
+        .titulo.mb-4(data-aos="fade-left")
+          img(src='@/assets/curso/icon.svg', alt='Imagen decorativa')
+          h5 Destrezas
+        p En principio, la comunicación es de carácter interpersonal, pero también se puede presentar la comunicación intragrupal u organizacional; por lo tanto, se hace necesario destrezas de comunicación. Entre ellas la más importante es contar con una actitud positiva y tener claro las palabras y acciones que se emitirán.
+        .col-lg-7
+        .titulo.mb-4(data-aos="fade-left")
+          img(src='@/assets/curso/icon.svg', alt='Imagen decorativa')
+          h5 Elementos de la comunicación
+        p.mb-0 La comunicación siempre incluye los siguientes elementos, que interactúan entre sí para realizar una comunicación efectiva, si falla alguno de los elementos falla la acción comunicativa:
 
+      .col-lg-5.col-6.mb-4.mb-lg-0.order-1.order-lg-2
+        figure(data-aos="zoom-in")
+          img.img-fluid(src="@/assets/curso/temas/tema4/img13.jpg" alt="Icono 1")
+    
+    .row.justify-content-center.align-items-center.mb-4
+      .col-lg-10
+        .titulo-sexto.color-primario(data-aos="fade-left")
+          h5 Figura 3.
+          i  Elementos de la comunicación
+    .row.justify-content-center.align-items-center.mb-4
+      .col-lg-10.movil(data-aos="fade-left")
+        figure
+          img(src='@/assets/curso/temas/tema4/img14_1.svg', alt='Figura 3 que relaciona los elementos que están presentes en la comunicación, los cuales parten del emisor quien origina la información y la organiza en un mensaje. Este mensaje se transmite a través de un canal, que puede ser verbal, escrito o no verbal, utilizando un código común que permite su comprensión, hasta llegar al receptor, quien interpreta el contenido y le da significado.')
+      .col-lg-10.desktop(data-aos="fade-left")
+        figure
+          img(src='@/assets/curso/temas/tema4/img14.svg', alt='Figura 3 que relaciona los elementos que están presentes en la comunicación, los cuales parten del emisor quien origina la información y la organiza en un mensaje. Este mensaje se transmite a través de un canal, que puede ser verbal, escrito o no verbal, utilizando un código común que permite su comprensión, hasta llegar al receptor, quien interpreta el contenido y le da significado.')
+
+    .row.justify-content-center.align-items-center.mb-4
+      .col-lg-10
+        p.mb-0 En el área de la salud, brindar una atención humanizada parte desde la actitud y aptitud del personal de salud-paciente y familia.
+    .row.justify-content-center.align-items-center.mb-4
+      .col-lg-10
+        .tarjeta--celeste.color-primario.p-2
+          i.fas.fa-quote-left
+          h5.mb-0 No importa cuán sofisticada sea la tecnología de salud recibida, la salud se alivia con tres simples elementos humanos: compasión, contacto y conversación. – Daniel Patrick Sulmasy
+          i.fas.fa-quote-right
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-10
+        p.mb-0 Es importante conocer las habilidades y barreras en la comunicación efectiva. Por consiguiente, se detallan en primera instancia las habilidades:
+    
+    .tarjeta--BMG4.p-5.mb-4
+      .tarjeta.tarjeta--gris.p-4
+        PasosB.color-acento-contenido
+          .row(titulo="")
+            .col-lg-6.col-10.mb-4.mb-lg-0
+              figure(data-aos="zoom-in")
+                img.img-fluid(src="@/assets/curso/temas/tema4/img15.jpg" alt="Imagen decorativa")
+            .col-lg-6.mb-4.mb-md-0
+              h5 Reflexión
+              p.mb-0 Cuando la persona emisora estructura el contenido del mensaje adecuándose a las circunstancias concretas del receptor, al entorno y buscando el canal de transmisión más adecuado.
+          .row(titulo="")
+            .col-lg-6.col-10.mb-4.mb-lg-0
+              figure(data-aos="zoom-in")
+                img.img-fluid(src="@/assets/curso/temas/tema4/img16.jpg" alt="Imagen decorativa")
+            .col-lg-6.mb-4.mb-md-0
+              h5 Hablar y escribir
+              p.mb-0 Habilidades exclusivas de la persona emisora, qué mensaje desea transmitir de forma oral o escrita.
+          .row(titulo="")
+            .col-lg-6.col-10.mb-4.mb-lg-0
+              figure(data-aos="zoom-in")
+                img.img-fluid(src="@/assets/curso/temas/tema4/img17.jpg" alt="Imagen decorativa")
+            .col-lg-6.mb-4.mb-md-0
+              h5 Intereses
+              p.mb-0 Habilidad de la persona emisora y receptora. Debe haber interés por comunicar de la persona emisora e interés de escuchar de la persona receptora.
+          .row(titulo="")
+            .col-lg-6.col-10.mb-4.mb-lg-0
+              figure(data-aos="zoom-in")
+                img.img-fluid(src="@/assets/curso/temas/tema4/img18.jpg" alt="Imagen decorativa")
+            .col-lg-6.mb-4.mb-md-0
+              h5 Nivel de conocimientos
+              p.mb-0 Tanto la persona emisora y la persona receptora, deben tener unos conocimientos similares para producir el mensaje y que este sea entendido.
+          .row(titulo="")
+            .col-lg-6.col-10.mb-4.mb-lg-0
+              figure(data-aos="zoom-in")
+                img.img-fluid(src="@/assets/curso/temas/tema4/img19.jpg" alt="Imagen decorativa")
+            .col-lg-6.mb-4.mb-md-0
+              h5 Sistema sociocultural
+              p.mb-0 Si la persona emisora y la persona receptora pertenecen a grupos sociales y culturas muy diferentes, puede haber complicaciones para entender el mensaje.
+          .row(titulo="")
+            .col-lg-6.col-10.mb-4.mb-lg-0
+              figure(data-aos="zoom-in")
+                img.img-fluid(src="@/assets/curso/temas/tema4/img20.jpg" alt="Imagen decorativa")
+            .col-lg-6.mb-4.mb-md-0
+              h5 Personalidad
+              p.mb-0 Las características personales son importantes en ambos extremos. La persona emisora y la persona receptora deben conocerse para ubicar el mensaje en su sentido exacto.
+          .row(titulo="")
+            .col-lg-6.col-10.mb-4.mb-lg-0
+              figure(data-aos="zoom-in")
+                img.img-fluid(src="@/assets/curso/temas/tema4/img21.jpg" alt="Imagen decorativa")
+            .col-lg-6.mb-4.mb-md-0
+              h5 Lectura y escucha
+              p.mb-0 Habilidades propias de la persona receptora para poder asimilar y comprender el mensaje, bien sea escrito u oral.
+    
+    .row.justify-content-center.align-items-center.mb-4
+      .col-lg-10
+        p.mb-0 Y como segunda instancia, se relacionan de igual o mayor importancia, las barrares:[
+    
+    SlyderF.mb-5(columnas="col-lg-6 col-xl-4")
+      .tarjeta.color-primario.p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/tema4/img22.svg' alt='AvatarTop')
+        h5.text-center Filtración
+        p.text-center Hace referencia a la manipulación de la información con la finalidad de que ésta sea vista de manera favorable por el receptor.
+      .tarjeta.color-primario.p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/tema4/img23.svg' alt='AvatarTop')
+        h5.text-center Percepción selectiva
+        p.text-center Surge cuando los receptores, durante el proceso de comunicación, reciben el mensaje de manera selectiva y lo decodifican.
+      .tarjeta.color-primario.p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/tema4/img24.svg' alt='AvatarTop')
+        h5.text-center Defensa
+        p.text-center Hace referencia a la acción-reacción; el sentimiento de amenaza hacia el individuo produce un resultado.
+      .tarjeta.color-primario.p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/tema4/img25.svg' alt='AvatarTop')
+        h5.text-center Lenguaje
+        p.text-center El significado de las palabras no está en la palabra, está en el ser mismo (de acuerdo con: la edad, la educación y los antecedentes culturales).
+      .tarjeta.color-primario.p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/tema4/img26.svg' alt='AvatarTop')
+        h5.text-center Deformación
+        p.text-center Es un problema semántico influenciado por la forma de pensar o la crianza.
+      .tarjeta.color-primario.p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/tema4/img27.svg' alt='AvatarTop')
+        h5.text-center Sobrecarga
+        p.text-center Tiene que ver con recibir demasiada información.
+      .tarjeta.color-primario.p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/tema4/img28.svg' alt='AvatarTop')
+        h5.text-center Sincronización
+        p.text-center Coordinación entre el tiempo y la información.
+      .tarjeta.color-primario.p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/tema4/img29.svg' alt='AvatarTop')
+        h5.text-center Falta de aceptación
+        p.text-center Negación.
+      .tarjeta.color-primario.p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/tema4/img30.svg' alt='AvatarTop')
+        h5.text-center Cortocircuito
+        p.text-centerOcurre cuando se salta algún nivel estructural de la organización.
+
+    .titulo.mb-4(data-aos="fade-left")
+      img(src='@/assets/curso/icon.svg', alt='Imagen decorativa')
+      h5 Comunicación efectiva en salud
+    p.mb-4 La comunicación tiene diversos canales que pueden verse afectados, a su vez, por múltiples factores mencionados anteriormente como el contexto, las personas, las emociones, entre otros. De allí la importancia de reflexionar en acciones propias que aporten a la comunicación efectiva en ámbitos de la salud. 
+
+    .tarjeta.color-acento-botones.p-3.mb-5(data-aos="fade-right")
+      .row.justify-content-around.align-items-center
+        .col-3.col-sm-2.col-lg-1
+          img(src="@/assets/curso/temas/tema4/img31.svg")
+        .col
+          .row.justify-content-between.align-items-center
+            .col.mb-sm-0.mb-4
+              h3.mb-1 Hacia una comunicación efectiva y humanista en ámbitos de salud
+              p Ingresando al siguiente artículo, le ayudará en la reflexión, reconociendo algunas situaciones y estrategias de uso práctico en el desempeño profesional o campo de acción.
+            .col-sm-auto
+              a.boton.color-acento-botones(:href="('/downloads/Hacia_una_comunicacion_efectiva.pdf')" target="_blank" type="application/pdf")
+                span Descargar
+                i.fas.fa-file-download
+      
 </template>
-
 <script>
 export default {
   name: 'Tema4',
